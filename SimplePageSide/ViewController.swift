b98590e6a889707ae20042a8d4d1dbcc15928cf0
Jitendra Agarwal <jitendrakumar_a@chmm120824.local>
2018-11-27 18:43:37 +0530

    Initial Commit

---
+++ SimplePageSide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplePageSide
+//
+//  Created by Jitendra Kumar Agarwal on 27/11/18.
+//  Copyright © 2018 Jitendra Kumar Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
